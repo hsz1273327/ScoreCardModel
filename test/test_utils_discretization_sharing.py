038653051e1dab066b670159c1769fa7ddc04dce
HUANG SIZHE <hsz1273327@gmail.com>
2017-05-18 18:31:37 +0800

    update
    update

--- test/test_utils_discretization_sharing.py
+++ test/test_utils_discretization_sharing.py
@@ -24,10 +24,10 @@ class TestDiscrete(TestCase):
         result = discrete(self.x,0)
         expected = [1,4,5,6,3,6,7,8,3,6,7,8,9,11]
         self.assertSequenceEqual(result,expected)
-
     def test_with_invalid_n(self):
-        result = discrete(self.x,9)
-        expected = [1, 3, 3, 5, 2, 5, 7, 8, 2, 5, 7, 8, 9, 9]
-
         with self.assertRaisesRegex(AttributeError,r"n must be a int") as a:
-        self.assertRaisesRegex(exc, r, fun, *args, **kwds)(result,expected)
+            discrete(self.x,"a")
+    def test_with_str_array(self):
+        result = discrete(self.y,0)
+        expected = [1,4,5,6,3,6,7,8,3,6,7,8,9,11]
+        self.assertSequenceEqual(result,expected)
